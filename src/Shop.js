2aa47147767691356ea34655b7b8dbf6f4c6aac4
Nikhilmehra123 <49524183+Nikhilmehra123@users.noreply.github.com>
2024-02-14 20:02:08 +0530

    finally redux is completed

--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,45 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
 
+// import { depositMoney, withdrawMoney } from "./state/action-creators";
 const Shop = () => {
+  const dispatch = useDispatch();
+  const balance = useSelector((state) => state.amount);
+
+  const { withdrawMoney, depositMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
   return (
     <div>
-      <h2>Buy Adidas Shoes 50Rs</h2>
-      <button className="btn btn-primary mx-2">-</button>
-      Add to cart
-      <button className="btn btn-primary mx-2">+</button>
+      <h2>Deposite/Withdraw Money</h2>
+      <button
+        className="btn btn-primary mx-2"
+        // onClick={() => {
+        //   dispatch(actionCreators.withdrawMoney(100));
+        // }}
+        // if we will use bindactionCreators then no need to do like we did in the above code
+        onClick={() => {
+          withdrawMoney(100);
+        }}
+      >
+        -
+      </button>
+      Update Balance ({balance})
+      <button
+        className="btn btn-primary mx-2"
+        //   onClick={() => {
+        //     dispatch(actionCreators.depositMoney(100));
+        //   }}
+        onClick={() => {
+          depositMoney(100);
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
